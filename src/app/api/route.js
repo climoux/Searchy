dcdabe3442913a32a6272690e9a924eef777bca2
Clément M. <clement.mnguy@outlook.fr>
2025-02-01 18:14:50 +0100

    Security fix

--- src/app/api/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-// Database
-import pool from '../../lib/db';
-
-// To handle a GET request to /api
-export async function GET(request) {
-    return NextResponse.json({}, { status: 200 });
-}
